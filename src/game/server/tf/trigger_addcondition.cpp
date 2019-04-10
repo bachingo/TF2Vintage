7586484dc7ac5eb990da758747c7159a75924c6b
Deathreus <csmoore96@yahoo.com>
2019-04-10 02:38:13 -0400

    Add `CTriggerRemoveTFPlayerCondition` to accompany it

--- src/game/server/tf/trigger_addcondition.cpp
+++ src/game/server/tf/trigger_addcondition.cpp
@@ -77,3 +77,56 @@ void CTriggerAddTFPlayerCondition::EndTouch( CBaseEntity *pOther )
 		}
 	}
 }
+
+
+class CTriggerRemoveTFPlayerCondition : public CBaseTrigger
+{
+	DECLARE_CLASS( CTriggerRemoveTFPlayerCondition, CBaseTrigger );
+
+public:
+	void	Spawn( void );
+	void	StartTouch( CBaseEntity *pOther );
+
+private:
+	DECLARE_DATADESC();
+
+	int m_nCondition;
+};
+
+
+BEGIN_DATADESC( CTriggerRemoveTFPlayerCondition )
+
+	DEFINE_KEYFIELD( m_nCondition, FIELD_INTEGER, "condition" ),
+
+END_DATADESC()
+
+LINK_ENTITY_TO_CLASS( trigger_remove_tf_player_condition, CTriggerRemoveTFPlayerCondition );
+
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CTriggerRemoveTFPlayerCondition::Spawn( void )
+{
+	BaseClass::Spawn();
+	InitTrigger();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CTriggerRemoveTFPlayerCondition::StartTouch( CBaseEntity *pOther )
+{
+	CTFPlayer *pPlayer = ToTFPlayer( pOther );
+	if ( !m_bDisabled && PassesTriggerFilters( pOther ) && pPlayer )
+	{
+		if ( m_nCondition == -1 )
+		{
+			pPlayer->m_Shared.RemoveAllCond( pPlayer );
+		}
+		else
+		{
+			pPlayer->m_Shared.RemoveCond( m_nCondition );
+		}
+	}
+}
