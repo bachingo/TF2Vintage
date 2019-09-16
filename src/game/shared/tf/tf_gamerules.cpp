c2650400a6f9a5f6e0a81fcd50af5a769c5d3541
Deathreus <csmoore96@yahoo.com>
2019-09-16 16:37:09 -0400

    Garbage pointer fix

--- src/game/shared/tf/tf_gamerules.cpp
+++ src/game/shared/tf/tf_gamerules.cpp
@@ -4702,7 +4702,8 @@ void CTFGameRules::Arena_SendPlayerNotifications( void )
 			{
 				for ( int j = 0; j < pTeam[i]->GetNumPlayers(); j++ )
 				{
-					pTFPlayer = ToTFPlayer( pTeam[i]->GetPlayer( i ) );
+					CBasePlayer *pPlayer = pTeam[ i ]->GetPlayer( j );
+					pTFPlayer = ToTFPlayer( pPlayer );
 					if ( pTFPlayer && pTFPlayer->IsReadyToPlay() && !pTFPlayer->IsHLTV() && !pTFPlayer->IsReplay() )
 					{
 						AddPlayerToQueue( pTFPlayer );
