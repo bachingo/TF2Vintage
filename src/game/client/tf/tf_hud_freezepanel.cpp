335ca14f6aaece674cd4c3f74920e96cbb0f3039
Deathreus <csmoore96@yahoo.com>
2019-09-22 01:01:49 -0400

    Null dereference catch
    This was the only null check that wasn't made

--- src/game/client/tf/tf_hud_freezepanel.cpp
+++ src/game/client/tf/tf_hud_freezepanel.cpp
@@ -754,17 +754,20 @@ void CTFFreezePanel::ShowNemesisPanel( bool bShow )
 
 void CTFFreezePanel::SetColorForTargetTeam( int iTeamNumber )
 {
-	switch ( iTeamNumber )
+	if( m_pFreezePanelBG )
 	{
-	case TF_TEAM_RED:
-		m_pFreezePanelBG->SetImage("../hud/freezecam_red_bg");
-		break;
-	case TF_TEAM_BLUE:
-		m_pFreezePanelBG->SetImage("../hud/freezecam_blue_bg");
-		break;
-	default:
-		m_pFreezePanelBG->SetImage("../hud/freezecam_black_bg");
-		break;
+		switch ( iTeamNumber )
+		{
+			case TF_TEAM_RED:
+				m_pFreezePanelBG->SetImage("../hud/freezecam_red_bg");
+				break;
+			case TF_TEAM_BLUE:
+				m_pFreezePanelBG->SetImage("../hud/freezecam_blue_bg");
+				break;
+			default:
+				m_pFreezePanelBG->SetImage("../hud/freezecam_black_bg");
+				break;
+		}
 	}
 }
 
