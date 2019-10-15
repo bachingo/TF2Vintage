54a2d8d6b363fe31f5dd2b3f60c92cd6f9415550
Deathreus <csmoore96@yahoo.com>
2019-10-15 03:07:24 -0400

    Shift TF_LOADOUT_SLOT_UTILITY

--- src/game/shared/in_buttons.h
+++ src/game/shared/in_buttons.h
@@ -36,6 +36,8 @@
 #define IN_BULLRUSH		(1 << 22)
 #define IN_GRENADE1		(1 << 23)	// grenade 1
 #define IN_GRENADE2		(1 << 24)	// grenade 2
-#define	IN_ATTACK3		(1 << 25)
+#define IN_ATTACK3		(1 << 25)
+
+#define IN_TYPING		(1 << 31)
 
 #endif // IN_BUTTONS_H
