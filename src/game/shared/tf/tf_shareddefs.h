1e02dcf4d928d0a5dd3dd0faebac2520223e9dfd
TF2V <spork.juct.2@gmail.com>
2019-10-18 01:55:44 -0400

    Define keyfields

--- src/game/shared/tf/tf_shareddefs.h
+++ src/game/shared/tf/tf_shareddefs.h
@@ -283,7 +283,6 @@ extern const char *g_aAmmoNames[];
 //-----------------------------------------------------------------------------
 // Weapons.
 //-----------------------------------------------------------------------------
-#define TF_PLAYER_WEAPON_COUNT		5
 #define TF_PLAYER_GRENADE_COUNT		2
 #define TF_PLAYER_BUILDABLE_COUNT	4
 
