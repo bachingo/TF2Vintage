495005f39f25d3a5e9d75288aaccffe771f89a1f
TF2V <spork.juct.2@gmail.com>
2020-01-10 21:21:38 -0500

    Update tf_hud_itemeffectmeter.cpp

--- src/game/client/tf/tf_hud_itemeffectmeter.cpp
+++ src/game/client/tf/tf_hud_itemeffectmeter.cpp
@@ -633,7 +633,7 @@ void CHudItemEffects::SetPlayer( void )
 			break;
 		case TF_CLASS_SOLDIER:
 			AddItemMeter( new CHudItemEffectMeterTemp<C_TFBuffItem>( "HudItemEffectMeter" ) );
-			AddItemMeter( new CHudItemEffectMeterTemp<C_TFRocketLauncher_Airstrike>( "HudItemEffectMeter" ) );
+			AddItemMeter( new CHudItemEffectMeterTemp<C_TFRocketLauncher_Airstrike>( "HudItemEffectMeter", "resource/UI/HudItemEffectMeter_Spy.res" ) ); // This shares the Diamondback's HUD location, strangely.
 			break;
 		case TF_CLASS_DEMOMAN:
 			AddItemMeter( new CHudItemEffectMeterTemp<C_TFSword>( "HudItemEffectMeter", "resource/UI/HudItemEffectMeter_Demoman.res" ) );
