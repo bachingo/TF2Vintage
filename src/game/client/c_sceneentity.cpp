05ebe6f2cb2c41565c6174f7d3e45bb39df55d85
TF2V <spork.juct.2@gmail.com>
2020-03-12 17:10:03 -0400

    Update c_sceneentity.cpp

--- src/game/client/c_sceneentity.cpp
+++ src/game/client/c_sceneentity.cpp
@@ -812,7 +812,7 @@ CChoreoScene *C_SceneEntity::LoadScene( const char *filename )
 	Q_FixSlashes( loadfile );
 
 	void *pBuffer = 0;
-	CChoreoScene *pScene = NULL;
+	CChoreoScene *pScene = nullptr;
 
 	int fileSize = filesystem->ReadFileEx( loadfile, "MOD", &pBuffer, true );
 	if (fileSize)
@@ -836,7 +836,7 @@ CChoreoScene *C_SceneEntity::LoadScene( const char *filename )
 	
 		if ( IsBufferBinaryVCD( (char*)pBuffer, fileSize ) )
 		{
-			pScene = new CChoreoScene( NULL );
+			pScene = new CChoreoScene( this );
 			CUtlBuffer buf( pBuffer, fileSize, CUtlBuffer::READ_ONLY );
 			if ( !pScene->RestoreFromBinaryBuffer( buf, loadfile, &g_ChoreoStringPool ) )
 			{
@@ -847,7 +847,7 @@ CChoreoScene *C_SceneEntity::LoadScene( const char *filename )
 		}
 	}
 
-	if( pScene )
+	if(pScene)
 	{
 		pScene->SetPrintFunc( Scene_Printf );
 		pScene->SetEventCallbackInterface( this );
