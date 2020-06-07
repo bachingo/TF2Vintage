9d44576c8b169acfc17d11e6d371b905f27fe934
TF2V <spork.juct.2@gmail.com>
2020-06-07 15:59:25 -0400

    Use rewritten ItemTintProxy
    -Uses the changed algorithm in 03b9540 without removing the model updates.

--- src/game/client/tf/c_tf_player.cpp
+++ src/game/client/tf/c_tf_player.cpp
@@ -1879,55 +1879,44 @@ public:
 	void OnBind( void *pC_BaseEntity )
 	{
 		Assert( m_pResult );
+		m_pResult->SetVecValue( 0, 0, 0 );
 
 		if ( !pC_BaseEntity )
-		{
-			m_pResult->SetVecValue( 0, 0, 0 );
 			return;
-		}
 
 		C_BaseEntity *pEntity = BindArgToEntity( pC_BaseEntity );
 		if ( !pEntity )
 			return;
 
-		CModelPanelModel *pPanelModel = dynamic_cast<CModelPanelModel *>( pEntity );
-		if ( pPanelModel )
+		bool bBlueTeam = pEntity->GetTeam() && pEntity->GetTeamNumber() == TF_TEAM_BLUE;
+		C_EconEntity *pEconEnt = dynamic_cast<C_EconEntity *>( pEntity );
+
+		CEconItemView *pItem = NULL;
+		if ( pEconEnt )
 		{
-			m_pResult->SetVecValue( pPanelModel->m_vecModelColor.x, pPanelModel->m_vecModelColor.y, pPanelModel->m_vecModelColor.z );
+			pItem = pEconEnt->GetItem();
+		}
+		else
+		{
+			C_EconEntity *pOwner = dynamic_cast<C_EconEntity *>( pEntity->GetOwnerEntity() );
+			if ( pOwner )
+				pItem = pOwner->GetItem();
+		}
+
+		if ( !pItem )
 			return;
-		}
 
-		C_TFPlayer *pPlayer = NULL;
-
-		C_TFRagdoll *pRagdoll = dynamic_cast<C_TFRagdoll *>( pEntity );
-		if ( pRagdoll )
+		uint nPaintRGB = pItem->GetModifiedRGBValue( bBlueTeam );
+		if( nPaintRGB != 0 )
 		{
-			EHANDLE hPlayer = pRagdoll->GetPlayerHandle();
-			pPlayer = ToTFPlayer( hPlayer.Get() );
-		}
+			float flPaint[3] ={
+				Clamp( ( nPaintRGB >> 8 ) / 255.0f, 0.0f, 1.0f ),
+				Clamp( ( nPaintRGB >> 4 ) / 255.0f, 0.0f, 1.0f ),
+				Clamp( ( nPaintRGB ) / 255.0f, 0.0f, 1.0f )
+			};
 
-		if ( !pPlayer )
-		{
-			C_TFWeaponBase *pWeapon = dynamic_cast<C_TFWeaponBase *>( pEntity );
-			if ( pWeapon )
-			{
-				pPlayer = ToTFPlayer( pWeapon->GetOwner() );
-			}
+			m_pResult->SetVecValue( flPaint[0], flPaint[1], flPaint[2] );
 		}
-
-		if ( !pPlayer )
-		{
-			pPlayer = ToTFPlayer( pEntity );
-		}
-
-		// Final check to see if it's a player
-		if ( pPlayer )
-		{
-			m_pResult->SetVecValue( pPlayer->m_vecPlayerColor.x, pPlayer->m_vecPlayerColor.y, pPlayer->m_vecPlayerColor.z );
-			return;
-		}
-
-		m_pResult->SetVecValue( 1, 1, 1 );
 	}
 };
 
