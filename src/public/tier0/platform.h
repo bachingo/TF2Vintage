4af40aed87c0b8f4ab672984e53fab2d5b1dd9bb
Deathreus <csmoore96@yahoo.com>
2025-04-18 23:13:29 -0400

    Update platform.h

--- src/public/tier0/platform.h
+++ src/public/tier0/platform.h
@@ -681,7 +681,7 @@ FIXME: Enable this when we no longer fear change =)
 #ifdef _LINUX
 	#define mallocsize( _p )	( malloc_usable_size( _p ) )
 
-	#include <math>
+	#include <math.h>
 	extern "C" {
 		double __exp_finite(double x) { return exp(x); }
 		double __log_finite(double x) { return log(x); }
