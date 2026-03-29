ae5bf7771e38e03e13c2d3c93ce792f104c701e9
TF2V <spork.juct.2@gmail.com>
2026-03-29 14:23:02 -0400

    Update viewrender.cpp

--- src/game/client/viewrender.cpp
+++ src/game/client/viewrender.cpp
@@ -274,6 +274,7 @@ private:
 };
 
 
+
 //-----------------------------------------------------------------------------
 //
 //-----------------------------------------------------------------------------
@@ -5340,6 +5341,10 @@ void CShadowDepthView::Draw()
 		render->Push3DView( (*this), VIEW_CLEAR_DEPTH, m_pRenderTarget, GetFrustum() );
 	}
 
+	pRenderContext.GetFrom(materials);
+	pRenderContext->PushRenderTargetAndViewport(m_pRenderTarget, m_pDepthTexture, 0, 0, m_pDepthTexture->GetMappingWidth(), m_pDepthTexture->GetMappingWidth());
+	pRenderContext.SafeRelease();
+
 	SetupCurrentView( origin, angles, VIEW_SHADOW_DEPTH_TEXTURE );
 
 	MDLCACHE_CRITICAL_SECTION();
@@ -5383,6 +5388,8 @@ void CShadowDepthView::Draw()
 		//Resolve() the depth texture here. Before the pop so the copy will recognize that the resolutions are the same
 		pRenderContext->CopyRenderTargetToTextureEx( m_pDepthTexture, -1, NULL, NULL );
 	}
+
+	pRenderContext->PopRenderTargetAndViewport();
 
 	render->PopView( GetFrustum() );
 
