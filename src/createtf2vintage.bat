471f0421705074fa2c71d1e1bbef0534850d9ba0
TF2V <spork.juct.2@gmail.com>
2026-04-04 00:50:38 -0400

    Update VPC

--- src/createtf2vintage.bat
+++ src/createtf2vintage.bat
@@ -1,2 +1,2 @@
-devtools\bin\vpc.exe /tf2vintage /define:SOURCESDK /define:TF +game /mksln TF2vintage.sln
+devtools\bin\vpc.exe /tf2vintage /define:SOURCESDK /define:TF /define:TF2VINTAGE +game /mksln TF2vintage.sln
 pause
