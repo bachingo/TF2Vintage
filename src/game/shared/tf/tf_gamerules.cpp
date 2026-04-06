fba5940db849a73a217b1af3a5da7fe646a58c87
TF2V <spork.juct.2@gmail.com>
2026-04-06 00:41:02 -0400

    Update tf_gamerules.cpp

--- src/game/shared/tf/tf_gamerules.cpp
+++ src/game/shared/tf/tf_gamerules.cpp
@@ -6,8 +6,6 @@
 //=============================================================================
 #include "cbase.h"
 #include "tf_gamerules.h"
-#include "tf_gamerules_convars.h"
-#include "tf_gamerules_era_internal.h"
 #include "tf_classdata.h"
 #include "ammodef.h"
 #include "KeyValues.h"
@@ -148,6 +146,12 @@
 #include "tf_duckleaderboard.h"
 
 #include "passtime_convars.h"
+
+//TF2V additions
+#include "tf_gamerules_convars.h"
+#include "tf_gamerules_era_internal.h"
+#include "tf_gamerules_era_members.h"
+#include "tf_gamerules_era_internal.h"
 
 #include "tier3/tier3.h"
 // memdbgon must be the last include file in a .cpp file!!!
