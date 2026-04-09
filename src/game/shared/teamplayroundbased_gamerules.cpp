2b6efa2f16b618a3705ea6e02c6c3784d613b92e
TF2V <spork.juct.2@gmail.com>
2026-04-08 23:59:37 -0400

    Reset respawns back to vanilla
    We'll address this later.

--- src/game/shared/teamplayroundbased_gamerules.cpp
+++ src/game/shared/teamplayroundbased_gamerules.cpp
@@ -647,18 +647,12 @@ void CTeamplayRoundBasedRules::LevelInitPostEntity( void )
 //-----------------------------------------------------------------------------
 float CTeamplayRoundBasedRules::GetRespawnTimeScalar( int iTeam )
 {
-	/*
-	// Vanilla: scale respawn time DOWN when player count is low
+	// For long respawn times, scale the time as the number of players drops
 	int iOptimalPlayers = 8;	// 16 players total, 8 per team
 
 	int iNumPlayers = GetGlobalTeam(iTeam)->GetNumPlayers();
 
 	float flScale = RemapValClamped( iNumPlayers, 1, iOptimalPlayers, 0.25, 1.0 );
-	return flScale;
-	*/
-	// Temporarily replaced with a simpler formula.
-	int iNumPlayers = GetGlobalTeam(iTeam)->GetNumPlayers();
-	float flScale = iNumPlayers * (1/8);
 	return flScale;
 }
 
