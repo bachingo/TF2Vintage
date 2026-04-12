278c8f46de7f8a2493d246dc0e6870c44b6ce2aa
TF2V <spork.juct.2@gmail.com>
2026-04-12 00:50:50 -0400

    Go back to 33

--- src/game/shared/shareddefs.h
+++ src/game/shared/shareddefs.h
@@ -258,7 +258,7 @@ enum CastVote
 	#define MAX_PLAYERS				33  // Absolute max players supported
 #endif
 */
-#define MAX_PLAYERS				65 // 17: TF2 limit, console. 25: TF2 limit, 2007-2008. 33: TF2 limit, 2008-2023. 101: TF2 limit, 2023-present.
+#define MAX_PLAYERS				33 // 17: TF2 limit, console. 25: TF2 limit, 2007-2008. 33: TF2 limit, 2008-2023. 101: TF2 limit, 2023-present.
 
 // Josh: Accounts for code that may index this array by an entindex
 // of player rather than the player index... :s
