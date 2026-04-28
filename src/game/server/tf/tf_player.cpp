f89eda44a921d822eda84fdaa97a62fce39dad53
TF2V <spork.juct.2@gmail.com>
2026-04-27 23:09:00 -0400

    Modify Australiums with the GoldRagdoll effect

--- src/game/server/tf/tf_player.cpp
+++ src/game/server/tf/tf_player.cpp
@@ -15845,6 +15845,10 @@ void CTFPlayer::CreateFeignDeathRagdoll( const CTakeDamageInfo& info, bool bGib,
 			{
 				 CALL_ATTRIB_HOOK_INT_ON_OTHER( info.GetWeapon(), iGoldRagdoll, set_turn_to_gold );
 			}
+			if ( !iGoldRagdoll && info.GetWeapon() ) // Second chance for Australiums
+			{
+				CALL_ATTRIB_HOOK_INT_ON_OTHER( info.GetWeapon(), iIsAustralium, is_australium_item );
+			}
 			pRagdoll->m_bGoldRagdoll = iGoldRagdoll != 0;
 
 			int iIceRagdoll = 0;
