5e64cbec4670f0a568f7e7f9b1808a6c08807700
TF2V <spork.juct.2@gmail.com>
2026-04-29 17:30:25 -0400

    Revert "Update createtf2vintage.bat"
    This reverts commit 73ac767a0aa4ce7b184442d907ac9430717ec168.

--- src/createtf2vintage.bat
+++ src/createtf2vintage.bat
@@ -1,5 +1,5 @@
 @echo off
 pushd "%~dp0"
-devtools\bin\vpc.exe /tf /define:SOURCESDK +everything /mksln tf2vintage.sln
+devtools\bin\vpc.exe /tf /define:LTCG /define:SOURCESDK +everything /mksln tf2vintage.sln
 popd
 pause
