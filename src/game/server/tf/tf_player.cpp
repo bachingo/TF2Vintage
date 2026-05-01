1880813740d98ab9795598a5160a9ff9e5a37b15
TF2V <spork.juct.2@gmail.com>
2026-05-01 12:42:03 -0400

    Update tf_player.cpp

--- src/game/server/tf/tf_player.cpp
+++ src/game/server/tf/tf_player.cpp
@@ -17242,6 +17242,10 @@ void CTFPlayer::CreateFeignDeathRagdoll( const CTakeDamageInfo& info, bool bGib,
 			{
 				 CALL_ATTRIB_HOOK_INT_ON_OTHER( info.GetWeapon(), iGoldRagdoll, set_turn_to_gold );
 			}
+			if ( !iGoldRagdoll )
+			{
+				CALL_ATTRIB_HOOK_INT_ON_OTHER( info.GetWeapon(), iGoldRagdoll, is_australium_item );
+			}
 			pRagdoll->m_bGoldRagdoll = iGoldRagdoll != 0;
 
 			int iIceRagdoll = 0;
