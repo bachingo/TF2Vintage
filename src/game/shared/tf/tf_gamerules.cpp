89846677840fb2d795354bd3b23d10cc2afa4d61
TF2V <spork.juct.2@gmail.com>
2026-05-06 23:47:45 -0400

    Hide IsAttackDefenseMode behind GAME_DLL

--- src/game/shared/tf/tf_gamerules.cpp
+++ src/game/shared/tf/tf_gamerules.cpp
@@ -3168,11 +3168,15 @@ bool CTFGameRules::StopWatchShouldBeTimedWin( bool bSkipForMultiSeries )
 //-----------------------------------------------------------------------------
 bool CTFGameRules::IsAttackDefenseMode( void )
 {
+#ifdef GAME_DLL
 	CTeamControlPointMaster *pMaster = g_hControlPointMasters.Count() ? g_hControlPointMasters[0] : NULL;
 	bool bRetVal = !HasMultipleTrains() && ( tf_gamemode_payload.GetBool() || ( pMaster && ( pMaster->PlayingMiniRounds() || pMaster->ShouldSwitchTeamsOnRoundWin() ) ) );
 
 	tf_attack_defend_map.SetValue( bRetVal );
 	return bRetVal;
+#else
+	return tf_attack_defend_map.GetBool();
+#endif
 }
 
 //-----------------------------------------------------------------------------
