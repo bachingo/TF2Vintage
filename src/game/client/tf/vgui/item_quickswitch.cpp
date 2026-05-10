72ba96134893879e27b3c7f73c0eb462fefca8e7
TF2V <spork.juct.2@gmail.com>
2026-05-10 00:04:49 -0400

    Update item_quickswitch.cpp

--- src/game/client/tf/vgui/item_quickswitch.cpp
+++ src/game/client/tf/vgui/item_quickswitch.cpp
@@ -32,7 +32,7 @@
 
 #define MAX_QUICKSWITCH_SLOTS 11
 
-//extern ConVar tf_respawn_on_loadoutchanges;
+extern ConVar tf_respawn_on_loadoutchanges;
 
 extern const char *g_szEquipSlotHeader[CLASS_LOADOUT_POSITION_COUNT];
 int g_SlotsToLoadoutSlotsPerClass[TF_LAST_NORMAL_CLASS][MAX_QUICKSWITCH_SLOTS] =
@@ -427,18 +427,10 @@ void CItemQuickSwitchPanel::CloseQS( void )
 
 	if ( m_bLoadoutHasChanged )
 	{
-
-#ifdef INVENTORY_VIA_WEBAPI
-		GTFGCClientSystem()->LocalInventoryChanged();
-#else
-		// UNDONE: we always do this to notify players of their loadout change. respawn is now checked server-side.
-		//if ( tf_respawn_on_loadoutchanges.GetBool() )
+		if ( tf_respawn_on_loadoutchanges.GetBool() )
 		{
 			// Tell the GC to tell server that we should respawn if we're in a respawn room
-			GCSDK::CGCMsg< MsgGCEmpty_t > msg(k_EMsgGCRespawnPostLoadoutChange);
-			GCClientSystem()->BSendMessage(msg);
 		}
-#endif
 
 		// Send the preset panel a msg so it can save the change
 		CEconItemView *pCurItemData = TFInventoryManager()->GetItemInLoadoutForClass( m_iClass, m_iSlot );
