b54b4b8c2ae443da953c610cebb495d16108d132
TF2V <spork.juct.2@gmail.com>
2026-05-09 23:57:12 -0400

    Update tf_weapon_spellbook.cpp

--- src/game/shared/tf/halloween/tf_weapon_spellbook.cpp
+++ src/game/shared/tf/halloween/tf_weapon_spellbook.cpp
@@ -512,13 +512,8 @@ void CEquipSpellbookNotification::Accept()
 	}
 
 	TFInventoryManager()->EquipItemInLoadout( pLocalPlayer->GetPlayerClass()->GetClassIndex(), LOADOUT_POSITION_ACTION, iItemId );
-#ifdef INVENTORY_VIA_WEBAPI
-	GTFGCClientSystem()->LocalInventoryChanged();
-#else
+	
 	// Tell the GC to tell server that we should respawn if we're in a respawn room
-	GCSDK::CGCMsg< ::MsgGCEmpty_t > msg(k_EMsgGCRespawnPostLoadoutChange);
-	GCClientSystem()->BSendMessage(msg);
-#endif
 
 	MarkForDeletion();
 }
