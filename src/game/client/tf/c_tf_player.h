dbf458fa7d08ee44c30657a2716bbcc4a07eec3c
TF2V <spork.juct.2@gmail.com>
2026-05-09 23:45:25 -0400

    Update c_tf_player.h

--- src/game/client/tf/c_tf_player.h
+++ src/game/client/tf/c_tf_player.h
@@ -308,7 +308,7 @@ public:
 	void			ForceUpdateObjectHudState( void );
 
 	bool			GetMedigunAutoHeal( void ){ return tf_medigun_autoheal.GetBool(); }
-	bool			ShouldAutoRezoom( void ){ return GetZoomMode() == 1; }
+	bool			ShouldAutoRezoom( void ){ return cl_autorezoom.GetBool(); }
 	bool			ShouldAutoReload( void ){ return cl_autoreload.GetBool(); }
 
 	void			GetTargetIDDataString( bool bIsDisguised, OUT_Z_BYTECAP(iMaxLenInBytes) wchar_t *sDataString, int iMaxLenInBytes, bool &bIsAmmoData, bool &bIsKillStreakData );
