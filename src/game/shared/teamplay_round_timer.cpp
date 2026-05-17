b2775fb278d4007f2e1850472cb64e7b2a46500e
TF2V <spork.juct.2@gmail.com>
2026-05-16 22:11:31 -0400

    Fix "" around era gated overtime/stalemate

--- src/game/shared/teamplay_round_timer.cpp
+++ src/game/shared/teamplay_round_timer.cpp
@@ -83,7 +83,7 @@ enum
 extern bool IsInCommentaryMode();
 
 #if defined( GAME_DLL ) && defined( TF_DLL )
-ConVar tf_overtime_nag( "tf_overtime_nag", 0, FCVAR_NOTIFY, "Announcer overtime nag." ); // Era Gated in TF2V.
+ConVar tf_overtime_nag( "tf_overtime_nag", "0", FCVAR_NOTIFY, "Announcer overtime nag." ); // Era Gated in TF2V.
 #endif
 
 #ifdef CLIENT_DLL
