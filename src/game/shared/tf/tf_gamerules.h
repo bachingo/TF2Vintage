4b8c37cb660fccceed3ff734b156d007e9a6ae60
TF2V <spork.juct.2@gmail.com>
2026-05-18 01:22:20 -0400

    Clean up tf2v_force_era_gamemode

--- src/game/shared/tf/tf_gamerules.h
+++ src/game/shared/tf/tf_gamerules.h
@@ -75,6 +75,7 @@ extern ConVar	tf_ctf_bonus_time;
 extern ConVar	tf_mvm_respec_enabled;
 extern ConVar	tf_spawn_glows_duration;
 extern ConVar 	tf2v_era;
+extern ConVar 	tf2v_force_era_gamemode;
 
 #ifdef GAME_DLL
 extern ConVar mp_tournament_prevent_team_switch_on_readyup;
@@ -1422,7 +1423,6 @@ public:
 	void SetTF2VEra( int nEra = tf2v_era.GetInt() ) // "What date do we want to change the server to?"
 	{ 
 		int iEraMin = TF2V_ERA_DAY_MIN;
-#ifdef GAME_DLL
 		if ( tf2v_force_era_gamemode.GetBool() )
 		{
 			// Gamemode check.
@@ -1474,11 +1474,19 @@ public:
 			
 			if ( tf_gamemode_pd.GetBool() )
 				iEraMin = TF2V_DAY_MAJOR_INVASION; // Player Destruction added in Invasion
+	
+			if ( MapHasPrefix( pszMapName, "vsh_" ) )
+				iEraMin = TF2V_DAY_SUMMER_2023; // Versus Saxton Hale added Summer 2023
 			
-			if ( IsCustomGameMode() )
-				iEraMin = TF2V_DAY_SUMMER_2023; // Community modes added Summer 2023
+			if ( MapHasPrefix( pszMapName, "zi_" ) )
+				iEraMin = TF2V_DAY_HALLOWEEN_2023; // Zombie Infection added Halloween 2023
+			
+			if ( MapHasPrefix( pszMapName, "tow_" ) )
+				iEraMin = TF2V_DAY_HALLOWEEN_2024; // Tug of War added Halloween 2024
+			
+			if ( MapHasPrefix( pszMapName, "htf_" ) )
+				iEraMin = TF2V_DAY_HALLOWEEN_2025; // Hold the Flag added Halloween 2024
 		}
-#endif
 
 		m_nTF2VEra = clamp( nEra, iEraMin, TF2V_ERA_DAY_MAX );
 		if ( m_nTF2VEra != nEra )
