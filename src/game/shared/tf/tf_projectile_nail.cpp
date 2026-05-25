48d4f5f088966996e9ec9ed32d9c360504e656be
TF2V <spork.juct.2@gmail.com>
2026-05-25 01:06:10 -0400

    Update tf_projectile_nail.cpp

--- src/game/shared/tf/tf_projectile_nail.cpp
+++ src/game/shared/tf/tf_projectile_nail.cpp
@@ -44,7 +44,7 @@ PRECACHE_REGISTER_FN(PrecacheSyringe);
 // CTFProjectile_Syringe
 //-----------------------------------------------------------------------------
 #define SYRINGE_GRAVITY		0.3f
-#define SYRINGE_VELOCITY	1000.0f
+#define SYRINGE_VELOCITY	2000.0f
 //-----------------------------------------------------------------------------
 // CTFProjectile_Nail (Unused)
 //-----------------------------------------------------------------------------
