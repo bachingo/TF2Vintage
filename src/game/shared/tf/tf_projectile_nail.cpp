787fdc98ea1c90352f54ba6cea68be236e8e0df2
TF2V <spork.juct.2@gmail.com>
2026-05-24 21:04:34 -0400

    Experiment 2: Increase Syringe Speed to match Super Nailgun

--- src/game/shared/tf/tf_projectile_nail.cpp
+++ src/game/shared/tf/tf_projectile_nail.cpp
@@ -43,8 +43,8 @@ PRECACHE_REGISTER_FN(PrecacheSyringe);
 //-----------------------------------------------------------------------------
 // CTFProjectile_Syringe
 //-----------------------------------------------------------------------------
-#define SYRINGE_GRAVITY		0.3f
-#define SYRINGE_VELOCITY	1000.0f
+#define SYRINGE_GRAVITY		0.2f
+#define SYRINGE_VELOCITY	2000.0f
 // Purpose:
 //-----------------------------------------------------------------------------
 CTFBaseProjectile *CTFProjectile_Syringe::Create( 
