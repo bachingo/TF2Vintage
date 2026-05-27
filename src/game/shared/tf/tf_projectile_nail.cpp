db8b7af56b29a6f058d5d1c7c1bab5696c0ed36a
TF2V <spork.juct.2@gmail.com>
2026-05-26 20:27:35 -0400

    Update tf_projectile_nail.cpp
    Revert the syringes from the Super Nailgun counterpart back to the original TF2 nailgun speed.
    Fun, but causes issues where too much time is spent fighting versus healing.

--- src/game/shared/tf/tf_projectile_nail.cpp
+++ src/game/shared/tf/tf_projectile_nail.cpp
@@ -44,14 +44,7 @@ PRECACHE_REGISTER_FN(PrecacheSyringe);
 // CTFProjectile_Syringe
 //-----------------------------------------------------------------------------
 #define SYRINGE_GRAVITY		0.3f
-#define SYRINGE_VELOCITY	2000.0f
-//-----------------------------------------------------------------------------
-// CTFProjectile_Nail (Unused)
-//-----------------------------------------------------------------------------
-#define NAIL_MODEL				"models/weapons/w_models/w_nail.mdl"
-#define NAIL_GRAVITY		0.3f
-#define NAIL_VELOCITY	1000.0f
-
+#define SYRINGE_VELOCITY	1000.0f
 // Purpose:
 //-----------------------------------------------------------------------------
 CTFBaseProjectile *CTFProjectile_Syringe::Create( 
