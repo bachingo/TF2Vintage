fd5c7ab5e716860745f34443918dda2f0cfe4cf1
TF2V <spork.juct.2@gmail.com>
2026-05-29 10:47:11 -0400

    Update tf_hud_itemeffectmeter.cpp

--- src/game/client/tf/tf_hud_itemeffectmeter.cpp
+++ src/game/client/tf/tf_hud_itemeffectmeter.cpp
@@ -241,10 +241,10 @@ void CHudItemEffectMeter::CreateHudElementsForClass( C_TFPlayer* pPlayer, CUtlVe
 
 		
 			if ( !FindAttribute( pItem, attrMeterType, &retval ) )
-				continue;
+				return;
 
 			if ( retval == ATTRIBUTE_METER_TYPE_NONE )
-				continue;
+				return;
 		
 			const char *pszLabelText = "";
 			CAttribute_String attrModule;
@@ -265,7 +265,7 @@ void CHudItemEffectMeter::CreateHudElementsForClass( C_TFPlayer* pPlayer, CUtlVe
 				hNewMeter->SetVisible( false );
 			}
 
-			continue;
+			return;
 		}
 	};
 
